484d618d22a0b42fe7206e246fc47e06825c826e
AvianLearning <iaradcliffe@hotmail.com>
2020-11-23 14:43:49 +0000

    Set up classes and created test files

--- src/main/java/staff/management/Manager.java
+++ src/main/java/staff/management/Manager.java
@@ -1,4 +1,17 @@
 package staff.management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String niNumber, double salary, String deptName) {
+        super(name, niNumber, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
 }
--- src/main/java/staff/techstaff/DatabaseAdmin.java
+++ src/main/java/staff/techstaff/DatabaseAdmin.java
@@ -1,4 +1,10 @@
 package staff.techstaff;
 
-public class DatabaseAdmin {
+import staff.Employee;
+
+public class DatabaseAdmin extends Employee {
+
+    public DatabaseAdmin(String name, String niNumber, double salary) {
+        super(name, niNumber, salary);
+    }
 }
